ab22eabb67bad2d709b09a2838234bffc389dadd
NTPhus <phu444080@gmail.com>
2025-02-13 15:27:58 +0700

    Tối ưu phầm tìm kiếm

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helper/filterStatus");
+const searchHelper = require("../../helper/search");
 //[GET] /admin/products
 
 module.exports.products = async (req, res) => {
@@ -7,29 +8,25 @@ module.exports.products = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
 
   let find = {
-    deleted: false
-  }
+    deleted: false,
+  };
 
-  if(req.query.status){
+  if (req.query.status) {
     find.status = req.query.status;
   }
 
-  let keyword = "";
-  if(req.query.keyword){
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, "i"); //Regex
-    find.title = regex;
+  //Search
+  const objectSearch = searchHelper(req.query);
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
-  
-  const products = await Product.find(find)
 
-    // console.log(products)
+  const products = await Product.find(find);
 
-    res.render("admin/pages/products/index", {
-      pageTitle: "Trang sản phẩm",
-      products: products,
-      filterStatus: filterStatus,
-      keyword: keyword
-    });
-  };
-  
+  res.render("admin/pages/products/index", {
+    pageTitle: "Trang sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: objectSearch.keyword,
+  });
+};
