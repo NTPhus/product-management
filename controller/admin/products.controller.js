2180e602e6952d64cf10eed36d62999dbb68b50f
NTPhus <phu444080@gmail.com>
2025-02-13 15:14:04 +0700

    Tối ưu phần bộ lọc

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,31 +1,10 @@
 const Product = require("../../models/product.model");
-
+const filterStatusHelper = require("../../helper/filterStatus");
 //[GET] /admin/products
 
 module.exports.products = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },{
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },{
-      name: "Ngừng hoạt động",
-      status: "inactive",
-      class: ""
-    }
-  ]
-  
-  if(req.query.status){
-    const index = filterStatus.findIndex(item => item.status === req.query.status);
-    filterStatus[index].class = "active";
-  }else{
-    const index = filterStatus.findIndex(item => item.status === "");
-    filterStatus[index].class = "active";
-  }
+  //Bộ lọc
+  const filterStatus = filterStatusHelper(req.query);
 
   let find = {
     deleted: false
