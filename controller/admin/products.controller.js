4ecca821514ffc154cae355e1014ae58100ff535
NTPhus <phu444080@gmail.com>
2025-02-18 21:13:09 +0700

    Chi tiết sản phẩm bên trang admin

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -174,3 +174,22 @@ module.exports.editPatch = async (req, res) => {
 
   res.redirect(`back`);
 }
+
+//[GET] /admin/products/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id
+    }
+    const product = await Product.findOne(find);
+  
+    res.render("admin/pages/products/detail", {
+      pageTitle: product.title,
+      product: product
+    });
+  } catch (error) {
+    req.flash("error", "Không tồn tại sản phẩm này");
+    res.redirect(`${prefixAdmin}/products`);
+  }
+}
