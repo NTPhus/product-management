673e35248bc00374ac57d7e9eafacc94e0101539
NTPhus <phu444080@gmail.com>
2025-02-21 20:46:35 +0700

    Fix lỗi số thứ tự

--- helper/createTree.js
+++ helper/createTree.js
@@ -1,19 +1,23 @@
+let count = 0;
 const createTree = (arr, parentId = "") => {
-    let tree = [];
-    arr.forEach((item) => {
-      if (item.parent_id === parentId) {
-        let newItem = item;
-        const children = createTree(arr, item.id);
-        if (children.length > 0) {
-          newItem.children = children;
-        }
-        tree.push(newItem);
+  let tree = [];
+  arr.forEach((item) => {
+    if (item.parent_id === parentId) {
+      count++;
+      let newItem = item;
+      newItem.index = count;
+      const children = createTree(arr, item.id);
+      if (children.length > 0) {
+        newItem.children = children;
       }
-    });
-    return tree;
-}
+      tree.push(newItem);
+    }
+  });
+  return tree;
+};
 
 module.exports.tree = (arr, parentId = "") => {
+  count = 0;
   let tree = createTree(arr);
   return tree;
 };
