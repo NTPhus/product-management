4d9b1fe3ff467b64709e5f9b40fff12d03085afb
NTPhus <phu444080@gmail.com>
2025-02-22 11:05:06 +0700

    Xây dựng danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,9 +3,11 @@ const systemConfig = require("../../config/system");
 const dashboardRouter = require("./dashboard.route");
 const productsRouter = require("./products.route");
 const productCategoryRouter = require("./product-category.route");
+const roleRouter = require("./role.route");
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(PATH_ADMIN + "/dashboard", dashboardRouter);
   app.use(PATH_ADMIN + "/products", productsRouter);
   app.use(PATH_ADMIN + "/product-category", productCategoryRouter);
+  app.use(PATH_ADMIN + "/roles", roleRouter);
 };
