051281d85f9df2d7ef24ccb358c8396333635814
NTPhus <phu444080@gmail.com>
2025-02-22 11:19:47 +0700

    cập nhật tính năng chỉnh sửa nhóm quyền

--- controller/admin/role.controller.js
+++ controller/admin/role.controller.js
@@ -26,5 +26,47 @@ module.exports.create = async (req, res) => {
 module.exports.createPost = async (req, res) => {
   const record = new Role(req.body);
   await record.save();
-  res.redirect(`${prefixAdmin}/roles`)
+  res.redirect(`${prefixAdmin}/roles`);
+};
+
+//[GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+
+    const data = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      data: data,
+    });
+  } catch (error) {
+    res.redirect(`${prefixAdmin}/roles`);
+  }
+};
+
+//[PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Role.updateOne(
+      {
+        _id: id,
+      },
+      req.body
+    );
+
+    req.flash("success", "Cập nhật quyền thành công");
+
+    res.redirect(`back`);
+  } catch (error) {
+    req.flash("error", "Cập nhật quyền thất bại");
+
+    res.redirect(`back`);
+  }
 };
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -7,6 +7,10 @@ router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
+router.get("/edit/:id", controller.edit);
+
 router.post("/create", controller.createPost);
+
+router.patch("/edit/:id", controller.editPatch);
 
 module.exports = router;
