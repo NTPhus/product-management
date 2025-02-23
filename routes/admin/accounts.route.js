10476d8b7cb9395f7fcbec30b724e7f8f65e7a2a
NTPhus <phu444080@gmail.com>
2025-02-23 19:16:12 +0700

    Tạo trang danh sách tài khoản

---
+++ routes/admin/accounts.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const upload = multer();
+
+const controller = require("../../controller/admin/accounts.controller");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const validate = require("../../validates/admin/account.validates");
+
+router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
+module.exports = router;
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,10 +4,12 @@ const dashboardRouter = require("./dashboard.route");
 const productsRouter = require("./products.route");
 const productCategoryRouter = require("./product-category.route");
 const roleRouter = require("./role.route");
+const accountRouter = require("./accounts.route");
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(PATH_ADMIN + "/dashboard", dashboardRouter);
   app.use(PATH_ADMIN + "/products", productsRouter);
   app.use(PATH_ADMIN + "/product-category", productCategoryRouter);
   app.use(PATH_ADMIN + "/roles", roleRouter);
+  app.use(PATH_ADMIN + "/accounts", accountRouter);
 };
