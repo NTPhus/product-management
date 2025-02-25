7205957fb35f290c7b050b4f409f1035614d9596
NTPhus <phu444080@gmail.com>
2025-02-25 20:49:10 +0700

    Hiển thị menu chung bên client

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -4,19 +4,20 @@ const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
-    deleted: false
-  }).sort({position: "desc"});
+    deleted: false,
+  }).sort({ position: "desc" });
 
-  const productsNew = products.map(item => {
-    item.priceNew = ((item.price*(100-item.discountPercentage))/100).toFixed(0)
+  const productsNew = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
     return item;
-  })
-
-  console.log(productsNew);
+  });
 
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products: productsNew
+    products: productsNew,
   });
 };
 
@@ -26,15 +27,15 @@ module.exports.detail = async (req, res) => {
     const find = {
       deleted: false,
       status: "active",
-      slug: req.params.slug
-    }
+      slug: req.params.slug,
+    };
     const product = await Product.findOne(find);
-  
+
     res.render("client/pages/products/detail", {
       pageTitle: product.title,
-      product: product
+      product: product,
     });
   } catch (error) {
     res.redirect(`/products`);
   }
-}
+};
--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -1,9 +1,12 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 const productsRoutes = require("./products.route");
 const homeRoutes = require("./home.route");
 
 module.exports = (app) => {
+  app.use(categoryMiddleware.category);
+
   app.get("/", homeRoutes);
 
   app.use("/products", productsRoutes);
-  
 };
