70bdfb4ad6859b3d17c7277c46bb4932b0dc321b
NTPhus <phu444080@gmail.com>
2025-03-04 22:14:38 +0700

    Chức năng chèn nhiều ảnh trong chat

--- models/chat.model.js
+++ models/chat.model.js
@@ -5,7 +5,7 @@ const chatSchema = new mongoose.Schema(
         user_id: String,
         // room_chat_id: String,
         content: String,
-        // image: Array,
+        images: Array,
         deleted: {
             type: Boolean,
             default: false
