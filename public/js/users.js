4f816bf7d21fe3e0f15db7d2a0255b0ad888512b
NTPhus <phu444080@gmail.com>
2025-03-06 16:43:21 +0700

    Cập nhật số lượng lời mời bên B khi gửi lời mời từ A

--- public/js/users.js
+++ public/js/users.js
@@ -1,55 +1,68 @@
 // Chức năng gửi yêu cầu
 const listBtnAddFriend = document.querySelectorAll("[btn-add-friend]");
-if(listBtnAddFriend.length > 0){
-    listBtnAddFriend.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.closest(".box-user").classList.add("add");
-            const userId = btn.getAttribute("btn-add-friend");
-            
-            socket.emit("CLIENT_ADD_FRIEND", userId);
-        })
-    })
+if (listBtnAddFriend.length > 0) {
+  listBtnAddFriend.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.closest(".box-user").classList.add("add");
+      const userId = btn.getAttribute("btn-add-friend");
+
+      socket.emit("CLIENT_ADD_FRIEND", userId);
+    });
+  });
 }
 // Hết Chức năng gửi yêu cầu
 
 // Chức năng hủy yêu cầu
 const listBtnCancelFriend = document.querySelectorAll("[btn-cancel-friend]");
-if(listBtnCancelFriend.length > 0){
-    listBtnCancelFriend.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.closest(".box-user").classList.remove("add");
-            const userId = btn.getAttribute("btn-cancel-friend");
-            
-            socket.emit("CLIENT_CANCEL_FRIEND", userId);
-        })
-    })
+if (listBtnCancelFriend.length > 0) {
+  listBtnCancelFriend.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.closest(".box-user").classList.remove("add");
+      const userId = btn.getAttribute("btn-cancel-friend");
+
+      socket.emit("CLIENT_CANCEL_FRIEND", userId);
+    });
+  });
 }
 // Hết Chức năng hủy yêu cầu
 
 // Chức năng Từ chối kết bạn
 const listBtnRefuseFriend = document.querySelectorAll("[btn-refuse-friend]");
-if(listBtnRefuseFriend.length > 0){
-    listBtnRefuseFriend.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.closest(".box-user").classList.remove("refuse");
-            const userId = btn.getAttribute("btn-refuse-friend");
-            
-            socket.emit("CLIENT_REFUSE_FRIEND", userId);
-        })
-    })
+if (listBtnRefuseFriend.length > 0) {
+  listBtnRefuseFriend.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.closest(".box-user").classList.remove("refuse");
+      const userId = btn.getAttribute("btn-refuse-friend");
+
+      socket.emit("CLIENT_REFUSE_FRIEND", userId);
+    });
+  });
 }
 // Hết Chức năng Từ chối kết bạn
 
 // Chức năng đồng ý kết bạn
 const listBtnAcceptFriend = document.querySelectorAll("[btn-accept-friend]");
-if(listBtnAcceptFriend.length > 0){
-    listBtnAcceptFriend.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.closest(".box-user").classList.remove("accept");
-            const userId = btn.getAttribute("btn-accept-friend");
-            
-            socket.emit("CLIENT_ACCEPT_FRIEND", userId);
-        })
-    })
+if (listBtnAcceptFriend.length > 0) {
+  listBtnAcceptFriend.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.closest(".box-user").classList.remove("accept");
+      const userId = btn.getAttribute("btn-accept-friend");
+
+      socket.emit("CLIENT_ACCEPT_FRIEND", userId);
+    });
+  });
 }
 // Hết Chức năng đồng ý kết bạn
+
+// SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+const badgeUsersAccept = document.querySelector("[badge-users-accept]");
+if (badgeUsersAccept) {
+    const userId = badgeUsersAccept.getAttribute("badge-users-accept");
+  socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
+    if(userId === data.lengthAcceptFriends){
+        badgeUsersAccept.innerHTML = data.lengthAcceptFriends;
+    }
+  });
+}
+
+// End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
