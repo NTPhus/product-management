2e2f89cf866ef35f3ca6fcef696f0785198ce414
NTPhus <phu444080@gmail.com>
2025-03-07 07:38:47 +0700

    Hiển thị tag online

--- controller/client/users.controller.js
+++ controller/client/users.controller.js
@@ -68,27 +68,53 @@ module.exports.request = async (req, res) => {
 
 //[GET] /users/accept
 module.exports.accept = async (req, res) => {
-    //Socket
-    usersSocket(res);
-    //End Socket
-  
-    const userId = res.locals.user.id;
-  
-    const myUser = await User.findOne({
-      _id: userId,
-    });
-  
-    const acceptFriends = myUser.acceptFriends;
-  
-    const users = await User.find({
-      _id: { $in: acceptFriends },
-      status: "active",
-      deleted: false,
-    }).select("id avatar fullname");
-  
-    res.render("client/pages/users/accept", {
-      pageTitle: "Lời mời đã nhận",
-      users: users,
-    });
-  };
-  
+  //Socket
+  usersSocket(res);
+  //End Socket
+
+  const userId = res.locals.user.id;
+
+  const myUser = await User.findOne({
+    _id: userId,
+  });
+
+  const acceptFriends = myUser.acceptFriends;
+
+  const users = await User.find({
+    _id: { $in: acceptFriends },
+    status: "active",
+    deleted: false,
+  }).select("id avatar fullname");
+
+  res.render("client/pages/users/accept", {
+    pageTitle: "Lời mời đã nhận",
+    users: users,
+  });
+};
+
+//[GET] /users/friends
+module.exports.friends = async (req, res) => {
+  //Socket
+  usersSocket(res);
+  //End Socket
+
+  const userId = res.locals.user.id;
+
+  const myUser = await User.findOne({
+    _id: userId,
+  });
+
+  const friendList = myUser.friendList;
+  const friendListId = friendList.map(user => user.user_id);
+
+  const friends = await User.find({
+    _id: { $in: friendListId },
+    status: "active",
+    deleted: false,
+  }).select("id avatar fullname statusOnline");
+
+  res.render("client/pages/users/friend", {
+    pageTitle: "Danh sách bạn bè",
+    friends: friends
+  });
+};
