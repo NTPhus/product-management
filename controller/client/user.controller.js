5933da2f275334c068840356c0a03a0432465247
NTPhus <phu444080@gmail.com>
2025-03-11 14:07:23 +0700

    Fix bug trùng token

--- controller/client/user.controller.js
+++ controller/client/user.controller.js
@@ -29,6 +29,7 @@ module.exports.registerPost = async (req, res) => {
   req.body.password = md5(req.body.password);
 
   const user = new User(req.body);
+  user.tokenUser = generateHelper(20);
   user.save();
 
   req.flash("success", "Tạo tài khoản thành công");
